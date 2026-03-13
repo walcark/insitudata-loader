450c82590a3e0c5ccbdd23a329bd32b185720e54
walcark <kevin.walcarius@cnes.fr>
2026-03-13 15:44:03 +0100

    config: remove useless dependencies.

--- src/insitudata_loader/satellites/__init__.py
+++ src/insitudata_loader/satellites/__init__.py
@@ -1,12 +1,7 @@
 from .satellite import Satellite
 from .srf import SRF
-from .geodes import GeodesCollectionType, get_pygeodes_config, search_items_in_geodes, SearchOnGeodes
 
 __all__ = [
     "Satellite",
     "SRF",
-    "GeodesCollectionType",
-    "get_pygeodes_config",
-    "search_items_in_geodes",
-    "SearchOnGeodes",
 ]
